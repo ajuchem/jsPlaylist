d039cf86cd1b8a079636887c66ab26df60ebaa53
ajuchem <ajuchem@daudt.com>
2016-03-24 11:17:38 -0600

    Add songs to playlist

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var playlist = new Playlist();
+
+var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
+var fieldsOfGold = new Song("Fields of Gold", "Sting", "3:40");
+var beautifulDay = new Song("Beautiful Day", "U2", "4:08");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:43");
+
+
+playlist.add(hereComesTheSun);
+playlist.add(fieldsOfGold);
+playlist.add(beautifulDay);
+playlist.add(walkingOnSunshine);
